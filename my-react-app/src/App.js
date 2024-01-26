9db2fc03e3994fc64bf511448aa62d4cc4464532
Attri-786 <141547558+Attri-786@users.noreply.github.com>
2024-01-26 12:46:24 +0500

    latest

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './header/Header';
-
 import Footer from './footer/Footer';
-
+import Home from './Home'; // Replace 'Home' with your actual component for the home section
+import About from './About'; // Replace 'About' with your actual component for the about section
 import './App.css';
 
 function App() {
   return (
-    <dive>
-    <Header />
-    <Footer />
-    </dive>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ my-react-app/src/Home/Home.js
@@ -0,0 +1,21 @@
+
+import './home.css'
+
+function Home(){
+    return(
+
+        <main>
+        <section class="home">
+            <div class="home-content">
+                <h1>Welcome to Our Website</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <a href="#learn-more">Learn More</a>
+            </div>
+        </section>
+
+      
+
+    </main>
+    );
+}
+export default Home;
