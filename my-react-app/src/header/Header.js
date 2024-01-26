8a0f237cdf1fabdadadfdd32a3d0355340595bac
Attri-786 <141547558+Attri-786@users.noreply.github.com>
2024-01-26 17:34:07 +0500

    update
    up

--- my-react-app/src/header/Header.js
+++ my-react-app/src/header/Header.js
@@ -10,7 +10,7 @@ return(
         </div>
         <nav>
             <ul>
-                <li><Link to="/">Home</Link></li>
+                <li><a href="/">Home</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Services</a></li>
                 <li><a href="#">Contact</a></li>
